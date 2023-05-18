404db7003d77e52ad46d599b378dfa16879a43e7
ShivamPansuria07 <Shivam.pansuria@gmail.com>
2023-05-17 19:04:19 -0700

    Serialization

--- src/_05_Serialization/MinecraftCreeper.java
+++ src/_05_Serialization/MinecraftCreeper.java
@@ -3,18 +3,20 @@ package _05_Serialization;
 /*
  * Complete this class so that it can be serialized.
  */
-public class MinecraftCreeper {
-	public String name;
-	public int xPosition;
-	public int yPosition;
-	public int zPosition;
-	public double health;
+import java.io.Serializable;
 
-	public MinecraftCreeper(String name, int x, int y, int z) {
-		this.name = name;
-		this.xPosition = x;
-		this.yPosition = y;
-		this.zPosition = z;
-		this.health = 100.0;
-	}
+public class MinecraftCreeper implements Serializable {
+    public String name;
+    public int xPosition;
+    public int yPosition;
+    public int zPosition;
+    public double health;
+
+    public MinecraftCreeper(String name, int x, int y, int z) {
+        this.name = name;
+        this.xPosition = x;
+        this.yPosition = y;
+        this.zPosition = z;
+        this.health = 100.0;
+    }
 }
